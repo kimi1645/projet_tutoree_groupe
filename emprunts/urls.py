266db4827dd3318de800307f3a20d9554c28747a
Sitraka <razafindrakotositrakiniaina@gmail.com>
2026-05-04 22:01:37 +0300

    Ajout url pour la recherche

--- emprunts/urls.py
+++ emprunts/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('',views.liste_emprunt, name= "listeEmprunt"  ),
     path('ajouter/', views.enregistrer_emprunt, name='enregistrerEmprunt'),
-    path('retourner/<int:id>/', views.retourner_emprunt, name='retournerEmprunt') 
+    path('retourner/<int:id>/', views.retourner_emprunt, name='retournerEmprunt'),
+    path('recherche/',views.recherche,name='search'), 
 ]
