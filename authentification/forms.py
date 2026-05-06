4165bd2a71ce1837f727222c9160bcf47b520679
Sitraka <razafindrakotositrakiniaina@gmail.com>
2026-05-06 22:50:39 +0300

    Ajout style

--- authentification/forms.py
+++ authentification/forms.py
@@ -4,5 +4,11 @@ from django import forms
 
 #Créer une formulaire d'authentification qui hérite de AuthenticationForm
 class FormulaireAuthentification(AuthenticationForm):
-    username = forms.CharField(widget=forms.TextInput)
-    password = forms.CharField(widget=forms.PasswordInput)
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder' : 'ex: john_doe',
+        'pattern' : "[0-9a-fA-F]{4,8}",
+        'class' : 'form-control'
+    }),label="Nom d'utilisateur")
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class' : 'form-control'
+    }), label="Mots de passe")
