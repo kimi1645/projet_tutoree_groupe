9a46bbd2ed173089750c89822d3e594b37873ec5
Sitraka <razafindrakotositrakiniaina@gmail.com>
2026-05-08 08:51:46 +0300

    Ajout views suppression

--- adherents/urls.py
+++ adherents/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("", adherent_views.liste_adherents, name="listeAdherent"),
     path("ajouter/", adherent_views.ajouter_adherent, name="ajouterAdherent"),
     path("modifier/<str:id>/", adherent_views.modifier_adherent, name="modifierAdherent"),
+    path("supprimer/<str:id>/", adherent_views.supprimer_adherent, name="supprimerAdherent"),
     path("verification/", adherent_views.verification, name="verification"),
     path("inscription/", adherent_views.inscription, name="inscription"),
     path("reservation", adherent_views.reservation_avec_detail, name="reservation"),
