65a12ed63184c5bbd7a3298694f8960ec7b22781
Sitraka <razafindrakotositrakiniaina@gmail.com>
2026-05-09 17:29:48 +0300

    Ajout signal pour supprimer compte lié à adherent lorsqu'on le supprime

---
+++ adherents/signals.py
@@ -0,0 +1,9 @@
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
+from .models import Adherent
+
+@receiver(post_delete, sender=Adherent)
+def supprimer_user_adherent(sender, instance, **kwargs):
+    if instance.user:
+        instance.user.delete()
+        
