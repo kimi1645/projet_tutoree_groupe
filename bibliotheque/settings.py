9d4e66430305c690f0f8873b2c703ebc5276f6c6
Sitraka <razafindrakotositrakiniaina@gmail.com>
2026-05-13 21:19:45 +0300

    Ajout ALLOWED_HOSTS

--- bibliotheque/settings.py
+++ bibliotheque/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['projet-tutoree-groupe-1.onrender.com', '127.0.0.1']
+ALLOWED_HOSTS = ['https://projet-tutoree-groupe-1.onrender.com', '127.0.0.1']
 
 
 # Application definition
